a80ff5876f6aef91b24e4a696791422c828c11a5
norimasa <murata19780521@gmail.com>
2022-07-06 14:28:53 +0900

    js課題追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//問題①
+
 const array = [2, 4, 7, 5, 4, 3, 8];
 
 
@@ -6,3 +8,20 @@ const new_array = array.filter(function(val, i, array){
 });
 
 console.log(new_array); 
+
+//問題②
+
+
+function isLeapYear(year){
+  if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
+
+let checkYear = 2021;
+if(isLeapYear(checkYear)){
+  console.log(checkYear + '年はうるう年です');
+}else{
+  console.log(checkYear + '年はうるう年ではありません');
+}
